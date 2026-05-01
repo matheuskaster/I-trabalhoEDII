49132f6b23f13b4f6a844b4692d9d921d961784b
matheuskaster <mathkaster@gmail.com>
2026-05-01 18:26:22 -0300

    01/05 fiz os dentro do svg as funções que vão desenhar o que o qry pede

--- include/svg.h
+++ include/svg.h
@@ -15,6 +15,38 @@ void abre_svg (FILE* arq_svg);
 /// @param q É um ponteiro que aponta para a quadra que será desenhada, no formato de um retângulo.
 void desenha_retangulo_svg (FILE* arq_svg, Quadra q);
 
+/// @brief Função que desenha um X vermelha, na âncora da quadra, com os 'x' e 'y' passados por parâmetro.
+/// @param arq_svg É um arquivo do tipo FILE* para acessar o arquivo, que conterá as ilustrações.
+/// @param x Número double, que representa a coordenada que ficará o X, no eixo 'x'.
+/// @param y Número double, que representa a coordenada que ficará o X, no eixo 'y'.
+void desenha_x_vermelho_svg (FILE* arq_svg, double x, double y);
+
+/// @brief Coloca o número de moradores próximo às faces - de cada face -, e no centro, o total.
+/// @param arq_svg É um arquivo do tipo FILE* para acessar o arquivo, que conterá as ilustrações.
+/// @param x Número double, que representa a coordenada que ficará a âncora do texto, no eixo 'x'.
+/// @param y Número double, que representa a coordenada que ficará a âncora do texto, no eixo 'y'.
+/// @param txto É a String que carrega o conteúdo que vai ser escrito no svg, no caso, o número.
+void desenha_numero_moradores_svg (FILE* arq_svg, double x, double y, char* cor, int tamanho, char* txto);
+
+/// @brief Função que desenha uma cruz vermelha, no endereço do morador que faleceu, com os 'x' e 'y' passados por parâmetro.
+/// @param arq_svg É um arquivo do tipo FILE* para acessar o arquivo, que conterá as ilustrações.
+/// @param x Número double, que representa a coordenada que ficará a âncora do cruz, no eixo 'x'.
+/// @param y Número double, que representa a coordenada que ficará a âncora do cruz, no eixo 'y'.
+void desenha_cruz_vermelha_svg(FILE* arq_svg, double x, double y);
+
+/// @brief Desenha quadrado vermelho no novo endereço da pessoa que possui esse cpf.
+/// @param arq_svg É um arquivo do tipo FILE* para acessar o arquivo, que conterá as ilustrações.
+/// @param x Número double, que representa a coordenada que ficará a âncora do quadrado, no eixo 'x'.
+/// @param y Número double, que representa a coordenada que ficará a âncora do quadrado, no eixo 'y'.
+/// @param cpf Número CPF identificador da pessoa, que foi despejada.
+void desenha_quadrado_vermelho_svg (FILE* arq_svg, double x, double y, char* cpf);
+
+/// @brief Desenha círculo preto, no endereço da pessoa que, infelizmente, perdeu a sua moradia.
+/// @param arq_svg É um arquivo do tipo FILE* para acessar o arquivo, que conterá as ilustrações.
+/// @param x Número double, que representa a coordenada que ficará o centro do círculo, no eixo 'x'.
+/// @param y Número double, que representa a coordenada que ficará o centro do círculo, no eixo 'y'.
+void desenha_circulo_preto_svg(FILE* arq_svg, double x, double y);
+
 /// @brief Escreve o rodapé do arquivo .svg, ou seja, é a tag </svg> que fecha o arquivo svg, indicando a realização com sucesso.
 /// @param arq_svg É um arquivo do tipo FILE* para acessar o arquivo, já inicializado.
 void fecha_svg (FILE* arq_svg);
--- src/svg.c
+++ src/svg.c
@@ -18,7 +18,33 @@ void abre_svg (FILE* arq_svg) {
 }
 
 void desenha_quadra_svg (FILE* arq_svg, Quadra q) {
-    fprintf(arq_svg, "<rect id=\"%d\" style=\"fill:%s;fill-opacity:0.5;stroke:%s\" height=\"%lf\" width=\"%lf\" y=\"%lf\" x=\"%lf\" stroke-width=\"%lf\" />\n", get_cep_quadra(q), get_cfill(q), get_cstrk(q), get_h_retangulo(q), get_w_retangulo(q), get_y_retangulo(q), get_x_retangulo(q), 1.0);
+    fprintf(arq_svg, "<rect cep=\"%s\" style=\"fill:%s;fill-opacity:0.5;stroke:%s\" height=\"%lf\" width=\"%lf\" y=\"%lf\" x=\"%lf\" stroke-width=\"%lf\" />\n", get_cep_quadra(q), get_cfill(q), get_cstrk(q), get_h_quadra(q), get_w_quadra(q), get_y_quadra(q), get_x_quadra(q), 1.0);
+}
+
+void desenha_x_vermelho_svg (FILE* arq_svg, double x, double y) {
+    double tamanho = 2.0;
+    fprintf(arq_svg, "\t<line x1=\"%lf\" y1=\"%lf\" x2=\"%lf\" y2=\"%lf\" stroke=\"red\" stroke-width=\"%lf\" />\n", x - tamanho, y - tamanho, x + tamanho, y + tamanho, 2.0);
+    fprintf(arq_svg, "\t<line x1=\"%lf\" y1=\"%lf\" x2=\"%lf\" y2=\"%lf\" stroke=\"red\" stroke-width=\"%lf\" />\n", x + tamanho, y - tamanho, x - tamanho, y + tamanho, 2.0);
+}
+
+void desenha_numero_moradores_svg (FILE* arq_svg, double x, double y, char* txto) {
+    fprintf(arq_svg, "\t<text x=\"%lf\" y=\"%lf\" fill=\"black\" font-size=\"2.0pt\" text-anchor=\"middle\" >%s</text>\n", x, y, txto);
+}
+
+void desenha_cruz_vermelha_svg(FILE* arq_svg, double x, double y) {
+    double tamanho = 2.0;
+    fprintf(arq_svg, "\t<line x1=\"%lf\" y1=\"%lf\" x2=\"%lf\" y2=\"%lf\" stroke=\"red\" stroke-width=\"%lf\" />\n", x, y - tamanho, x, y + tamanho, 1.5);
+    fprintf(arq_svg, "\t<line x1=\"%lf\" y1=\"%lf\" x2=\"%lf\" y2=\"%lf\" stroke=\"red\" stroke-width=\"%lf\" />\n", x - tamanho, y, x + tamanho, y, 1.5);
+}
+
+void desenha_quadrado_vermelho_svg (FILE* arq_svg, double x, double y, char* cpf) {
+    double altura_largura = 2.0;
+    fprintf(arq_svg, "\t<rect x=\"%lf\" y=\"%lf\" width=\"%lf\" height=\"%lf\" stroke=\"red\" fill=\"none\" opacity=\"%lf\" stroke-width=\"%lf\" />\n", x, y, altura_largura, altura_largura, 0.5, 1.5);
+    fprintf(arq_svg, "\t<text x=\"%lf\" y=\"%lf\" fill=\"red\" font-size=\"3px\" text-anchor=\"middle\" >%s</text>\n", x + 2.0, y + altura_largura - 2.0, cpf);
+}
+
+void desenha_circulo_preto_svg(FILE* arq_svg, double x, double y) {
+    fprintf(arq_svg, "<circle cx=\"%lf\" cy=\"%lf\" r=\"%lf\" stroke=\"black\" fill=\"black\" opacity=\"%lf\" stroke-width=\"%lf\" />\n", x, y, 2.0, 0.5, 1.5);
 }
 
 void fecha_svg(FILE* arq_svg) {
